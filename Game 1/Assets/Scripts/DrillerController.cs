cafb1c4844123f05bb25e1401e58080b2d305906
malouf <ryammouni@gmail.com>
2014-06-12 14:04:03 +0200

    Added the animation of Driller and 2 planets

---
+++ Game 1/Assets/Scripts/DrillerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrillerController : MonoBehaviour {
+
+	private Animator animator;
+	private bool IsDrilling = false;
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var vertical = Input.GetAxis("Vertical");
+		if (vertical < 0)
+			IsDrilling = true;			
+		else
+			IsDrilling = false;
+		
+		animator.SetBool ("IsDrilling", IsDrilling);
+	}
+}
