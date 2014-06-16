ca8eba3e7a3ea5e60eb7f9804a21a628c1dfec62
malouf <ryammouni@gmail.com>
2014-06-16 14:03:35 +0200

    Added a spawner

---
+++ Game 1/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+	
+	public float spawnTime = 5f;		// The amount of time between each spawn.
+	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public float objectScaleMin = 1f;
+	public float objectScaleMax = 1f;
+	public GameObject[] objectToSpawn;		// Array of enemy prefabs.
+	
+	void Start ()
+	{
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+	
+	void Spawn ()
+	{
+		// Instantiate a random enemy.
+		int index = Random.Range(0, objectToSpawn.Length);
+		GameObject SpawnedObject = (GameObject)Instantiate(objectToSpawn[index], transform.position, transform.rotation);
+		float scale = Random.Range(objectScaleMin, objectScaleMax);
+		SpawnedObject.transform.localScale = new Vector3 (scale, scale, 1);
+	}
+}
